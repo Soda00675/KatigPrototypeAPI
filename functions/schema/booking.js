aa42a5467d324042f6b7fd8d9c1709250b0784cb
JohnKenneth <fetizanankennetho@gmail.com>
2024-06-05 15:33:42 +0800

    update

--- functions/schema/booking.js
+++ functions/schema/booking.js
@@ -13,6 +13,10 @@ const bookingSchema = new Schema({
     unique: true, // Ensures bookingId is unique
     index: true // Adds an index to the bookingId field
   },
+  boatId: {
+    type: String,
+    required: true
+  },
   dateTime: {
     type: Date,
     required: true
@@ -29,5 +33,4 @@ const bookingSchema = new Schema({
   }
 });
 
-module.exports = mongoose.model('Booking', bookingSchema);
-ZZ
+module.exports = bookingSchema;
